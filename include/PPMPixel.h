07d4059a04fa2868d0e404bf114f501802d5805f
inxanedev <inxaneninja@gmail.com>
2021-02-16 09:23:55 +0000

    Fixed tabs to 4 spaces

--- include/PPMPixel.h
+++ include/PPMPixel.h
@@ -3,12 +3,12 @@
 #include <cstdint>
 
 namespace ppm {
-	class Pixel {
-		public:
-			char r, g, b;
-			// Constructs new pixel with specified R, G and B values.
-			Pixel(uint32_t r, uint32_t g, uint32_t b);
-			// Constructs new default black pixel
-			Pixel();
-	};
+    class Pixel {
+        public:
+            char r, g, b;
+            // Constructs new pixel with specified R, G and B values.
+            Pixel(uint32_t r, uint32_t g, uint32_t b);
+            // Constructs new default black pixel
+            Pixel();
+    };
 }
--- src/PPMPixel.cc
+++ src/PPMPixel.cc
@@ -1,11 +1,11 @@
 #include "PPMPixel.h"
 namespace ppm {
-	Pixel::Pixel(uint32_t r, uint32_t g, uint32_t b) {
-		this->r = r;
-		this->g = g;
-		this->b = b;
-	}
-	Pixel::Pixel() {
-		r = 0, g = 0, b = 0;
-	}
+    Pixel::Pixel(uint32_t r, uint32_t g, uint32_t b) {
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+    Pixel::Pixel() {
+        r = 0, g = 0, b = 0;
+    }
 }
